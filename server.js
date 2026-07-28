429164810674e5c9ba788785e1ca39d8dfd1b669
Eric Carvalho <eric.dcvieira@gmail.com>
2026-07-27 23:36:12 -0300

    Testes de query params, route params e body params

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios/:abacate', (req, res) => {
+    console.log(req)
     res.send('Olá rota acessada com sucesso!');
 });
+
+app.post('/usuarios', (req, res) => {
+    console.log(req)
+    res.send('Olá rota acessada com sucesso! AQUI É POST');
+});
+
 
 app.listen(3000);
