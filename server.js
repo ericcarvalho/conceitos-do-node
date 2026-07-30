05e870dd2136076479ace8aaf8769b561b68aed0
Eric Carvalho <eric.dcvieira@gmail.com>
2026-07-29 21:16:57 -0300

    CRUD no mongoDG com JS+Express

--- server.js
+++ server.js
@@ -1,20 +1,66 @@
 import express from 'express';
+import { PrismaClient } from './generated/prisma/client.js';
 
 const app = express();
 app.use(express.json());
 
-const users = [];
+//const users = [];
+const prisma = new PrismaClient();
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async(req, res) => {
     //console.log(req)
+    const users = await prisma.user.findMany()
     res.status(200).json(users)
 });
 
-app.post('/usuarios', (req, res) => {
-   // console.log(req.body)
-   users.push(req.body);
-    res.status(201).json({ message: 'Usuário criado com sucesso!' });
+app.post('/usuarios', async (req, res) => {
+    // console.log(req.body)
+    //users.push(req.body);
+    //PROMISSE quando usa await e async
+    const user = await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    });
+    
+        res.status(201).json(user);
+
 });
 
+app.put('/usuarios/:id', async (req, res) => {
+    // console.log(req.body)
+    //users.push(req.body);
+    //PROMISSE quando usa await e async
+    const user = await prisma.user.update({
+        where:{
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    });
+    
+        res.status(201).json(user);
+
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    // console.log(req.body)
+    //users.push(req.body);
+    //PROMISSE quando usa await e async
+    const user = await prisma.user.delete({
+        where:{
+            id: req.params.id
+        }
+    });
+    
+        res.status(200).json({ message: `Usuário ${user.name} deletado com sucesso!` });
+
+});
 
 app.listen(3000);
+
